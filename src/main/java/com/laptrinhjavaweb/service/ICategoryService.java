3cf086363278bc31be20be0835b79ae57942dbdb
TranMinhMan040702 <mantm040702@gmail.com>
2022-08-29 23:44:21 +0700

    jdbc cơ bản, cài đặt mysql lấy data từ mysql

--- src/main/java/com/laptrinhjavaweb/service/ICategoryService.java
+++ src/main/java/com/laptrinhjavaweb/service/ICategoryService.java
@@ -1,9 +1,9 @@
-package com.laptrinhjavaweb.dao;
+package com.laptrinhjavaweb.service;
 
 import java.util.List;
 
 import com.laptrinhjavaweb.model.CategoryModel;
 
-public interface ICategoryDAO {
+public interface ICategoryService {
 	List<CategoryModel> findAll();
 }
--- src/main/webapp/views/web/home.jsp
+++ src/main/webapp/views/web/home.jsp
@@ -7,7 +7,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Trang chủ</title>
 	<!-- css -->
-	
+	<%@ include file="/common/web/link-css.jsp" %>
 </head>
 <body>
 	<%@ include file="/common/web/header.jsp" %>
